1e38b67f0866740a229506ebf3824c609ba77f16
shun-999 <71618433+shun-999@users.noreply.github.com>
2020-12-10 17:16:57 +0900

    Add files via upload

--- pos-system.py
+++ pos-system.py
@@ -1,4 +1,5 @@
 ### 商品クラス
+import pandas as pd
 class Item:
     def __init__(self,item_code,item_name,price):
         self.item_code=item_code
@@ -30,22 +31,31 @@ class Order:
         for item,name,price  in zip(self.item_order_list, self.item_order_name, self.item_order_price):
             print("商品コード:{},商品名:{},価格:{}".format(item,name,price))
   
+def master_recog(file):
+    item_master=[]
+    df = pd.read_csv(file)
+    for item,name,price in zip(list(df["商品コード"]),list(df["商品名"]),list(df["価格"])):
+        item_master.append(Item(str(item),name,price))
+    return item_master
+
+
 
 ### メイン処理
 def main():
     # マスタ登録
-    item_master=[]
-    item_master.append(Item("001","りんご",100))
-    item_master.append(Item("002","なし",120))
-    item_master.append(Item("003","みかん",150))
-
+    item_master = master_recog("./master.csv")
+    #item_master = []
+    #item_master.append(Item("001","りんご",100))
+    #item_master.append(Item("002","なし",120))
+    #item_master.append(Item("003","みかん",150))
 
 
     # オーダー登録
     order = Order(item_master)
-    order.add_item_order("001")
-    order.add_item_order("002")
-    order.add_item_order("003")
+    #order.add_item_order("001")
+    #order.add_item_order("002")
+    #order.add_item_order("003")
+    order.add_item_order(input("商品コードを入力してください >>"))
     
     # オーダー表示
     order.view_item_list()
